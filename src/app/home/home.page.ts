f005ad90d4b881ba9d3211f787a691a920a03852
alimedianto <if23.alimedianto@mhs.ubpkarawang.ac.id>
2025-05-04 21:35:12 +0800

    edit home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,11 +9,28 @@ import { Component } from '@angular/core';
 export class HomePage {
   username: string = '';
 
+  items = [
+    { id: 'burger', name: 'BURGER', img: 'assets/makanan/burger.jpg', time: '30 min', sold: '200', count: 0 },
+    { id: 'pizza', name: 'PIZZA', img: 'assets/makanan/pizza.jpg', time: '30 min', sold: '200', count: 0 },
+    { id: 'chicken', name: 'AYAM KRIUK', img: 'assets/makanan/chiken.jpg', time: '30 min', sold: '200', count: 0 },
+    { id: 'spageti', name: 'SPAGETI', img: 'assets/makanan/spageti.jpg', time: '30 min', sold: '200', count: 0 }
+  ];
+
   ngOnInit() {
     const savedUser = localStorage.getItem('loggedInUser');
     if (savedUser) {
       const user = JSON.parse(savedUser);
       this.username = user.username;
     }
+  }
+
+  increment(id: string) {
+    const item = this.items.find(i => i.id === id);
+    if (item) item.count++;
+  }
+
+  decrement(id: string) {
+    const item = this.items.find(i => i.id === id);
+    if (item && item.count > 0) item.count--;
   }
 }
